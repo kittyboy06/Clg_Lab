b225374b0db46e2e1b7c76afb5aee52bab84ac5d
Kittyboy06 <afsalahmed2006@gmail.com>
2025-05-21 17:11:13 +0530

    .

--- Lab/7b - Copy.py
+++
@@ -1,7 +0,0 @@
-def string_length(s):
-    if s == "":
-        return 0
-    return 1 + string_length(s[1:])
-string=input("enter a string:")
-length=string_length(string)
-print("Length of the string:",length)
